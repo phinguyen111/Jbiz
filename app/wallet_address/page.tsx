58d17ae29641df392c1784595380f8a059621a10
phinguyen111 <125988580+phinguyen111@users.noreply.github.com>
2024-11-21 15:46:47 +0700

    Update page.tsx

--- app/wallet_address/page.tsx
+++ app/wallet_address/page.tsx
@@ -295,21 +295,21 @@ const ETHERSCAN_API_KEY = "RQ1E2Y5VTM4EKCNZTDHD58UCIXMPD34N1J";
     }
 };
 const fetchMultichainData = async () => {
-    setLoading(true);
-    setError(null);
-    try {
-        const response = await axios.get(`https://deep-index.moralis.io/api/v2/${walletAddress}/erc20/${tokenAddress}`, {
-            headers: {
-                'X-API-Key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjdlYWZlZGFlLWQxYzktNGZiNS05OWJkLTRiNmU0ODMzMGM3YiIsIm9yZ0lkIjoiNDE2NTM4IiwidXNlcklkIjoiNDI4MTQ4IiwidHlwZSI6IlBST0pFQ1QiLCJ0eXBlSWQiOiI5MWE1M2YyZS00OGYxLTRiOTEtOTAyYy1kMTM3ZGFiOWQ0YTYiLCJpYXQiOjE3MzE4NzMzMDUsImV4cCI6NDg4NzYzMzMwNX0.eO0Dk38ZaLy-HgaUAYU-tou4ObTfdWQU9JBLMTQ_Dmo', 
-            },
-        });
-        setTokenHoldings(prev => [...prev, ...response.data]); // Combine with existing token holdings
-    } catch (err) {
-        console.error('Error fetching multichain data:', err);
-        setError('Error fetching data');
-    } finally {
-        setLoading(false);
-    }
+  setLoading(true);
+  setError(null);
+  try {
+    const response = await axios.get(https://deep-index.moralis.io/api/v2/${walletAddress}/erc20/${tokenAddress}, {
+      headers: {
+      'X-API-Key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjdlYWZlZGFlLWQxYzktNGZiNS05OWJkLTRiNmU0ODMzMGM3YiIsIm9yZ0lkIjoiNDE2NTM4IiwidXNlcklkIjoiNDI4MTQ4IiwidHlwZSI6IlBST0pFQ1QiLCJ0eXBlSWQiOiI5MWE1M2YyZS00OGYxLTRiOTEtOTAyYy1kMTM3ZGFiOWQ0YTYiLCJpYXQiOjE3MzE4NzMzMDUsImV4cCI6NDg4NzYzMzMwNX0.eO0Dk38ZaLy-HgaUAYU-tou4ObTfdWQU9JBLMTQ_Dmo',
+    },
+      });
+  setTokenHoldings(prev => [...prev, ...response.data]); // Combine with existing token holdings
+} catch (err) {
+  console.error('Error fetching multichain data:', err);
+  setError('Error fetching data');
+} finally {
+  setLoading(false);
+}
 };
 const fetchAddressInfo = async (address: string) => {
   setLoading(true);
